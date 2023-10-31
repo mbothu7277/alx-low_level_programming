1a630d5bdbff20d32ba125fd7f667f9f2d502d04
mbothu7277 <kaguongombothu@gmail.com>
2023-10-31 09:45:42 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);           
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
