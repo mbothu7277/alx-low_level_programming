9aee179e9c028dd283484af5c1995bc1af8fbdde
mbothu7277 <kaguongombothu@gmail.com>
2023-11-09 03:02:15 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,14 @@
-#include <iostream>
-#include <string>
-void printName(const std::string& name) 
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - print name using pointer to function
+* @name: string to add
+* @f: pointer to function
+* Return: nothing
+**/
+void print_name(char *name, void (*f)(char *))
 {
-std::cout << "Hello, " << name << "!" << std::endl;
-}
-int main() {
-std::string name = "John Doe";
-printName(name);
-return 0;
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
